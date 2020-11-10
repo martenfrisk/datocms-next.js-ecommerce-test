d8243e8381a132df45301bbd3d03e54d83e3d7c6
Mårten Frisk <marten.frisk@askas.se>
2020-11-10 11:50:30 +0100

    Remove next auth to reduce bundle size

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import '../styles/index.css'
-import { Provider } from 'next-auth/client'
 import { CartProvider } from '../components/cart/cart-context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <CartProvider>
-        <Component {...pageProps} />
-      </CartProvider>
-    </Provider>
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
   )
 }
 
