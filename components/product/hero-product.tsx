b5ffc76a0d28981ec0c8466eda0df11f74e5592b
Mårten Frisk <marten.frisk@askas.se>
2021-02-10 16:39:41 +0100

    Hide hero img on mobile

--- components/product/hero-product.tsx
+++ components/product/hero-product.tsx
@@ -6,6 +6,7 @@ import { useDrag } from 'react-dnd'
 import { useCart, useDispatchCart } from '@/cart/cart-context'
 import { ProductType, ItemTypes } from '@/lib/types'
 import ImageOpt from '@/components/image'
+// import { updateCart } from '@/lib/airapi'
 
 export default function HeroProduct({
 	productName,
@@ -36,6 +37,14 @@ export default function HeroProduct({
 		setVisible(true)
 	}
 
+	// const carttest = async (product: string, quantity: number) => {
+	// 	const user = window.localStorage.getItem('user_data')
+	// 	if (user) {
+	// 		await updateCart(JSON.parse(user), product, quantity.toString())
+	// 		// console.log(res)
+	// 	}
+	// }
+
 	const [{ isDragging }, drag] = useDrag({
 		item: { cover, type: ItemTypes.PRODUCT },
 		options: { dropEffect: 'copy' },
@@ -63,15 +72,14 @@ export default function HeroProduct({
 	return (
 		<>
 			<div
-				className="absolute top-0 flex justify-center w-full mt-20 overflow-hidden border-b-4 border-blue-700 shadow-sm sm:mt-0 bg-gradient-to-r from-blue-700 to-blue-800"
+				className="absolute top-0 flex justify-center w-full mt-20 overflow-hidden border-b-4 border-blue-700 shadow-sm sm:-mt-10 bg-gradient-to-r from-blue-600 to-blue-800"
 				style={{
 					height: '50vh',
 					minHeight: '380px',
 					maxHeight: '425px',
 				}}
 			>
-				<div className="w-full">
-
+				<div className="hidden w-full sm:block">
 					{/* <Image
 						src={`/images/zoom/${heroimg}.jpg`}
 						alt={`Cover for ${productName}`}
@@ -83,30 +91,51 @@ export default function HeroProduct({
 						className="z-0 object-cover opacity-50 hero"
 					/> */}
 					<picture>
-						<source srcSet={`${process.env.NODE_ENV === 'development' ? '' : ''}/images/zoom/${heroimg}.avif`} type="image/avif" />
-						<source srcSet={`${process.env.NODE_ENV === 'development' ? '' : ''}/images/zoom/${heroimg}.webp`} type="image/webp" />
+						<source
+							srcSet={`${
+								process.env.NODE_ENV === 'development' ? '' : ''
+							}/images/zoom/${heroimg}.avif`}
+							type="image/avif"
+						/>
+						<source
+							srcSet={`${
+								process.env.NODE_ENV === 'development' ? '' : ''
+							}/images/zoom/${heroimg}.webp`}
+							type="image/webp"
+						/>
 						<img
-							src={`${process.env.NODE_ENV === 'development' ? '' : ''}/images/zoom/${heroimg}.jpg`}
+							src={`${
+								process.env.NODE_ENV === 'development' ? '' : ''
+							}/images/zoom/${heroimg}.jpg`}
 							alt={`Cover for ${productName}`}
 							loading="eager"
 							className="z-0 mx-auto opacity-50"
 							width={1440}
 							height={809}
-							style={{ clipPath: 'polygon(0 0, 85% 0, 100% 100%, 15% 100%)', maxWidth: '1440px'	}}
+							style={{
+								clipPath: 'polygon(0 0, 85% 0, 100% 100%, 15% 100%)',
+								maxWidth: '1440px',
+							}}
 						/>
 					</picture>
 				</div>
-
 			</div>
 			<section
-				className={`flex flex-wrap justify-center max-w-6xl mx-auto items-center w-full overflow-hidden text-white ${!heroimg && 'bg-navy-700'}`}
+				className={`flex flex-wrap justify-center max-w-6xl mx-auto items-center w-full overflow-hidden text-white ${
+					!heroimg && 'bg-navy-700'
+				}`}
 			>
-				<div ref={drag} style={{ opacity }} className="z-10 flex justify-center w-1/2 shadow-lg cursor-move sm:w-1/4">
+				<div
+					ref={drag}
+					style={{ opacity }}
+					className="z-10 flex justify-end w-1/2 shadow-lg cursor-move"
+				>
+					{/* Fix this img layout shifting */}
 					<ImageOpt
 						src={cover}
 						height={300}
 						width={300}
-						className="w-full rounded-lg"
+						className="shadow-lg rounded-2xl"
 						notLazy
 					/>
 					{/* <Image
@@ -119,16 +148,14 @@ export default function HeroProduct({
 					/> */}
 				</div>
 
-				<div className="z-0 flex flex-col flex-wrap items-center justify-center w-full px-2 mb-16 sm:mt-4 sm:w-1/2 lg:w-1/3">
+				<div className="z-0 flex flex-col flex-wrap items-center justify-center w-full px-2 mt-2 mb-2 sm:mb-16 sm:mt-4 sm:w-1/2 lg:w-1/3">
 					<Link href={`/products/${encodeURIComponent(artnr)}`}>
-						<a
-							className="text-6xl font-normal leading-none text-center hover:underline"
-						>
+						<a className="text-4xl font-normal leading-none text-center sm:text-6xl hover:underline">
 							<span>{productName}</span>
 						</a>
 					</Link>
 					<p
-						className="px-4 py-3 m-2 mt-4 text-base leading-normal text-center text-white bg-white bg-opacity-25 border border-gray-400 rounded-md shadow-xl"
+						className="px-2 py-3 m-2 mt-4 text-sm leading-normal text-center text-white bg-white bg-opacity-25 border border-gray-400 rounded-md shadow-xl sm:px-4 sm:text-base"
 						style={{ backdropFilter: 'blur(10px)' }}
 					>
 						{descriptionShort}
@@ -136,5 +163,5 @@ export default function HeroProduct({
 				</div>
 			</section>
 		</>
-	);
+	)
 }
